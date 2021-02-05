79cbc9ad72ed39cb2077951926c84b2b5335ab08
An Doan <minh.an.doan@gmail.com>
2021-02-04 22:23:53 -0800

    Using FormatMessage from string using va_list parameter for arguments.

---
+++ chapter1/FormatMessageString/FormatMessageString/FormatMessageString.cpp
@@ -0,0 +1,51 @@
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#include <windows.h>
+#include <stdio.h>
+
+LPWSTR GetFormattedMessage(LPCWSTR pMessage, ...);
+
+int main(void)
+{
+    LPWSTR pBuffer = NULL;
+    LPCWSTR pMessage = L"%1!*.*s! %3 %4!*s!";
+
+    // The variable length arguments correspond directly to the format
+    // strings in pMessage.
+    pBuffer = GetFormattedMessage(pMessage, 4, 3, L"Bill", L"Bob", 4, L"Bill");
+    if (pBuffer)
+    {
+        // Buffer contains "  Bi Bob   Bill".
+        wprintf(L"Formatted message: %ws\n", pBuffer);
+        LocalFree(pBuffer);
+    }
+    else
+    {
+        wprintf(L"Format message failed with 0x%x\n", GetLastError());
+    }
+}
+
+// Formats a message string using the specified message and variable
+// list of arguments.
+LPWSTR GetFormattedMessage(LPCWSTR pMessage, ...)
+{
+    LPWSTR pBuffer = NULL;
+
+    va_list args = NULL;
+    va_start(args, pMessage);
+
+    FormatMessage(FORMAT_MESSAGE_FROM_STRING |
+        FORMAT_MESSAGE_ALLOCATE_BUFFER,
+        pMessage,
+        0,
+        0,
+        (LPWSTR)&pBuffer,
+        0,
+        &args);
+
+    va_end(args);
+
+    return pBuffer;
+}
