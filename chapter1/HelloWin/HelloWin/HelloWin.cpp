a45e99ce7ac31a705c1b07b32700e43036b824a5
An Doan <minh.an.doan@gmail.com>
2021-02-06 20:17:59 -0800

    Implement non-deprecated way to find Windows version.

--- chapter1/HelloWin/HelloWin/HelloWin.cpp
+++ chapter1/HelloWin/HelloWin/HelloWin.cpp
@@ -1,6 +1,5 @@
 // HelloWin.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
-#define BUILD_WINDOWS
 #include "pch.h"
 
 int main()
@@ -20,8 +19,18 @@ int main()
     //Min process address : 0x00010000
     //Max process address : 0xFFFEFFFF
 
+    // DEPRECATED GET VERSION FUNCTION
+
     OSVERSIONINFO vi = { sizeof(vi) };
     ::GetVersionEx(&vi);
 
     printf("Version: %d.%d.%d\n", vi.dwMajorVersion, vi.dwMinorVersion, vi.dwBuildNumber);
+
+    // NEW GET VERSION FUNCTION
+    // Based on KUSER_SHARED_DATA data struct mapped to every process to same address
+    auto sharedUserData = (BYTE*)0x7FFE0000; // address to data struct
+    ULONG* majorVer = (ULONG*)(sharedUserData + 0x26c); // major version offset
+    ULONG* minorVer = (ULONG*)(sharedUserData + 0x270); // major version offset
+    ULONG* buildNum = (ULONG*)(sharedUserData + 0x260); // major version offset (Windows 10)
+    printf("Version: %d.%d.%d\n", *majorVer, *minorVer, *buildNum);
 }
--- chapter1/HelloWin/HelloWin/pch.h
+++ chapter1/HelloWin/HelloWin/pch.h
@@ -2,7 +2,10 @@
 #ifndef PCH_H
 #define PCH_H
 
+#define BUILD_WINDOWS
+
 #include <Windows.h>
 #include <stdio.h>
+#include<VersionHelpers.h>
 
 #endif //PCH_H
